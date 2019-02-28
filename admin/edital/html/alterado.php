59afbb7abccb2722e9c003d4dc0a50f34b99e606
gabrielbuquer <gabrielb12332@hotmail.com>
2019-02-28 17:17:25 -0300

    Update alterado.php

--- admin/edital/html/alterado.php
+++ admin/edital/html/alterado.php
@@ -5,7 +5,7 @@
     <div class="main-content">
     	<h1 class="main-title">Alterou!</h1>
     	<p class="main-form-legend">O edital foi alterado com sucesso.</p>
-    	<a class="main-form-back" href="./listaDeEditais.php">Voltar</a>
+    	<a class="main-form-back" href="./lista">Voltar</a>
     </div>
 </main>
 <?php
