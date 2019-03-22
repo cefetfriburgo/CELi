005b9cae8cf5b3e1f549f392452022891cabc5b3
Leonardo Pinto <Leozinho.guilherme@hotmail.com>
2019-03-22 14:44:19 -0300

    Update listar.php

--- admin/aluno/html/listar.php
+++ admin/aluno/html/listar.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 0);
 error_reporting(E_ALL);
 
-require "../control/listaalunos.php";
+require "../control/listar.php";
 
 $tagTitle = "Lista de alunos";
 require_once "../../../arquivosfixos/headerFooter/header.php";
