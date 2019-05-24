faeb5a6a6675ed0feb45b143a53b6c8f90ae5ab1
Leonardo Pinto <Leozinho.guilherme@hotmail.com>
2019-05-24 14:21:40 -0300

    Update erro.php

--- concurso/html/erro.php
+++ concurso/html/erro.php
@@ -1,9 +1,8 @@
 <div id="main">
 	<div class="main-content">
-		<h1 class="main-title">Inválido</h1>
-		<p class="main-form-legend">Houve algum problema no formulário. Por favor, verifique os
-			campos e tente novamente.</p>
-		<a class="main-form-back" href="./inscricao.php">Voltar</a>
+		<h1 class="main-title">Participação inválida</h1>
+		<p class="main-form-legend">Constatamos que um ou mais dados pessoais preenchidos por você já foi enviado para este concurso.</p>
+		<a class="main-form-back" href="/concursos">Voltar</a>
 	</div>
 </div>
 
