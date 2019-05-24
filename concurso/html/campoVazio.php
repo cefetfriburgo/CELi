b8008f457bbc6c6eb3b145c47b6119a1a8dacd14
Leonardo Pinto <Leozinho.guilherme@hotmail.com>
2019-05-24 14:19:04 -0300

    Update campoVazio.php

--- concurso/html/campoVazio.php
+++ concurso/html/campoVazio.php
@@ -2,7 +2,7 @@
 	<div class="main-content">
 		<h1 class="main-title">Inválido</h1>
 		<p class="main-form-legend">Há campos do formulário em branco. Por favor, verifique e tente novamente.</p>
-		<a class="main-form-back" href="./inscricao.php">Voltar</a>
+		<a class="main-form-back" href="/concursos">Voltar</a>
 	</div>
 </div>
 
