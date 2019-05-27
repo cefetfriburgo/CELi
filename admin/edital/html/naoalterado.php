1db578c320622983f9c12f73a516217bab6ff5b2
Leonardo Pinto <Leozinho.guilherme@hotmail.com>
2019-05-27 14:11:35 -0300

    Update naoalterado.php

--- admin/edital/html/naoalterado.php
+++ admin/edital/html/naoalterado.php
@@ -3,9 +3,9 @@
 ?>
 <main id="main">
 	<div class="main-content">
-		<h1 class="main-title">Não Alterou!</h1>
+		<h1 class="main-title">Não Alterado</h1>
 		<p class="main-form-legend">Houve um erro inexperado ao inserir o formulário. Por favor, verifique as informações fornecidas e tente novamente. Se persistir o erro, contate o administrador do sistema.</p>
-		<a class="main-form-back" href="./listaDeEditais.php">Voltar</a>
+		<a class="main-form-back" href="/admin/edital/lista">Voltar</a>
 	</div>
 </main>
 <?php
