7c1ec6c11e87b11f93082d16bc405f157f31151e
Leonardo Pinto <Leozinho.guilherme@hotmail.com>
2019-05-27 14:10:27 -0300

    Update naoinseriu.php

--- admin/edital/html/naoinseriu.php
+++ admin/edital/html/naoinseriu.php
@@ -4,7 +4,7 @@ require_once "../../../arquivosfixos/headerFooter/header.php";
 ?>
 <main id="main">
 <div class="main-content">
-	<h1 class="main-title">Não inseriu</h1>
+	<h1 class="main-title">Não inserido</h1>
 	<p class="main-form-legend">Houve um erro inexperado ao inserir o
 		formulário. Por favor, verifique as informações fornecidas e tente
 		novamente. Se persistir o erro, contate o administrador do sistema.</p>
